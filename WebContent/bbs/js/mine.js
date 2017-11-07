edc0c4ca593124259f2f61e6a1b01ef822e845a6
HWK <HeyWeCome@163.com>
2017-11-07 21:34:17 +0800

    Mine页面样式修改

--- WebContent/bbs/js/mine.js
+++ WebContent/bbs/js/mine.js
@@ -1,5 +1,5 @@
-var openID = getURLName("openID");
-//var openID = "oNwnMv75CAORzVhzjt-7J8lWSQtA";
+//var openID = getURLName("openID");
+var openID = "oNwnMv75CAORzVhzjt-7J8lWSQtA";
 $(function(){
 	if(openID != null){
 		initUser();
@@ -48,15 +48,15 @@ function addInvitationDiv(data,dataPic){
 	// data[0] id;data[1]:头像， data[2]:内容，data[3]:时间，data[4]：评论量,data[5]:用户名
 	// dataPic[0] id;dataPic[1]:所有图片base64编码,
 	var InvitationDiv = "";
-	InvitationDiv += "<div class='content' id='content'>";
+	InvitationDiv += "<div class='content' id='content' >";
 	InvitationDiv += "<div class='head'>" 
 				  +     "<div class='headImage'><img src='data:image/jpeg;base64,"+ data[1] +"' style='width: 80%;height: 80%;margin-left: 10%;margin-top: 10%;'></div>"
 				  +     "<div class='headNANDT'><div class='headName'>"+ data[5] + "</div>"
 				  +		"<div class='headTime'>"+ data[3]+"</div></div>"
 				  +	 "</div>";
-	InvitationDiv += "<div class='contentText'>"+ data[2] +"</div>";
-	InvitationDiv += "<div class='contentPicture'>"+ getPictrue(data[0],dataPic)  +"</div>";
-	InvitationDiv += "<div class='foot'>"+"<span>当前评论："+data[4] +"| </span>"+ "<span onclick='reply("+ data[0] +")'>回复 | </span>" + "<span onclick='particulars("+ data[0] +")'> 详情</span>" + "</div>";
+	InvitationDiv += "<div class='contentText' style='margin-left:70px;'>"+ data[2] +"</div>";
+	InvitationDiv += "<div class='contentPicture' style='margin-left:70px;margin-top:10px;'>"+ getPictrue(data[0],dataPic)  +"</div>";
+	InvitationDiv += "<div class='foot' style='margin-left:70px;margin-top:10px;'>"+"<span>当前评论："+data[4] +"| </span>"+ "<span onclick='reply("+ data[0] +")'>回复 | </span>" + "<span onclick='particulars("+ data[0] +")'> 详情</span>" + "</div>";
 	InvitationDiv += "</div>";
 	$("#allInvitation").append(InvitationDiv);
 }
