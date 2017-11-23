d41624a2f0d909d12e4aeb52c294b82205273f3a
1411573025 <1411573025@qq.com>
2017-11-23 23:12:15 +0800

    Signed-off-by: 1411573025 <1411573025@qq.com>增加信息量

--- src/org/jiaowei/controllers/PersonalDesignController.java
+++ src/org/jiaowei/controllers/PersonalDesignController.java
@@ -1,6 +1,15 @@
 package org.jiaowei.controllers;
 
 import java.awt.Robot;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -213,9 +222,9 @@ public class PersonalDesignController {
     	}else if(section.equals("G65渝湘段")){
     	 message = "http://www.cq96096.cn/videoImg/nextList?parentId=1003&type=2&openId=oPxXujqkhacHTudxFlVug9QIt_4s";
     	}else if(section.equals("G50沪渝高速")){
-    		message="http://www.cq96096.cn/videoImg/nextList?parentId=1003&type=2&openId=oPxXujqkhacHTudxFlVug9QIt_4s";	
+    		message="http://www.cq96096.cn/videoImg/nextList?parentId=1008&type=2&openId=oPxXujqkhacHTudxFlVug9QIt_4s";	
     	}else {
-			message="http://www.cq96096.cn/videoImg/nextList?parentId=1009&type=2&openId=oPxXujqkhacHTudxFlVug9QIt_4s";
+			message="http://www.cq96096.cn/videoImg/list?code=003DrKMk1I9lzl0jeYNk1GmyMk1DrKMX&state=123";
 		}
     	System.out.println("道路信息截图");
     	if(delayTimeNUM>=0){
@@ -263,6 +272,101 @@ public class PersonalDesignController {
         }
  
     }
+    public static String uploadMedia(String accessToken, String type, String mediaFileUrl) {
+    	String MEDIA_URL = "http://file.api.weixin.qq.com/cgi-bin/media/upload?access_token=ACCESS_TOKEN&type=TYPE";
+        StringBuffer resultStr = null;
+        //拼装url地址
+        String mediaStr = MEDIA_URL.replace("ACCESS_TOKEN", accessToken).replace("TYPE", type);
+        System.out.println("mediaStr:" + mediaStr);
+        URL mediaUrl;
+        try {
+            String boundary = "----WebKitFormBoundaryOYXo8heIv9pgpGjT";
+            URL url = new URL(mediaStr);
+            HttpURLConnection urlConn = (HttpURLConnection)url.openConnection();
+            //让输入输出流开启
+            urlConn.setDoInput(true);
+            urlConn.setDoOutput(true);
+            //使用post方式请求的时候必须关闭缓存
+            urlConn.setUseCaches(false);
+            //设置请求头的Content-Type属性
+            urlConn.setRequestProperty("Content-Type", "multipart/form-data; boundary="+boundary);
+            urlConn.setRequestMethod("POST");
+            //获取输出流，使用输出流拼接请求体
+            OutputStream out = urlConn.getOutputStream();
+
+            //读取文件的数据,构建一个GET请求，然后读取指定地址中的数据
+            mediaUrl = new URL(mediaFileUrl);
+            HttpURLConnection mediaConn = (HttpURLConnection)mediaUrl.openConnection();
+            //设置请求方式
+            mediaConn.setRequestMethod("GET");
+            //设置可以打开输入流
+            mediaConn.setDoInput(true);
+            //获取传输的数据类型
+            String contentType = mediaConn.getHeaderField("Content-Type");
+            //将获取大到的类型转换成扩展名
+            String fileExt = judgeType(contentType);
+            //获取输入流，从mediaURL里面读取数据
+            InputStream in = mediaConn.getInputStream();
+            BufferedInputStream bufferedIn = new BufferedInputStream(in);
+            //数据读取到这个数组里面
+            byte[] bytes = new byte[1024];
+            int size = 0;
+            //使用outputStream流输出信息到请求体当中去
+            out.write(("--"+boundary+"\r\n").getBytes());
+            out.write(("Content-Disposition: form-data; name=\"media\";\r\n"
+                    + "filename=\""+(new Date().getTime())+fileExt+"\"\r\n"
+                            + "Content-Type: "+contentType+"\r\n\r\n").getBytes());
+            while( (size = bufferedIn.read(bytes)) != -1) {
+                out.write(bytes, 0, size);
+            }
+            //切记，这里的换行符不能少，否则将会报41005错误
+            out.write(("\r\n--"+boundary+"--\r\n").getBytes());
+
+            bufferedIn.close();
+            in.close();
+            mediaConn.disconnect();
+
+            InputStream resultIn = urlConn.getInputStream();
+            InputStreamReader reader = new InputStreamReader(resultIn);
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            String tempStr = null;
+            resultStr = new StringBuffer(); 
+            while((tempStr = bufferedReader.readLine()) != null) {
+                resultStr.append(tempStr);
+            }
+            bufferedReader.close();
+            reader.close();
+            resultIn.close();
+            urlConn.disconnect();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return resultStr.toString();
+    }
+    /**
+     * 通过传过来的contentType判断是哪一种类型
+     * @param contentType 获取来自连接的contentType
+     * @return
+     */
+    public static String judgeType(String contentType) {
+        String fileExt = "";
+        if("image/jpeg".equals(contentType)) {
+            fileExt = ".jpg";
+        } else if("audio/mpeg".equals(contentType)) {
+            fileExt = ".mp3";
+        } else if("audio/amr".equals(contentType)) {
+            fileExt = ".amr";
+        } else if("video/mp4".equals(contentType)) {
+            fileExt = ".mp4";
+        } else if("video/mpeg4".equals(contentType)) {
+            fileExt = ".mp4";
+        }
+        return fileExt;
+    }
+
     
     
     
